e2e4cf37281e0c568953aad0f8d4ae76f67e159b
mmanylov <maksim.manylov@ramax.ru>
2020-07-13 15:23:25 +0300

    APP [FIX]: заменил декоратор json_response на json_service

--- views.py
+++ views.py
@@ -4,19 +4,18 @@ from aiohttp.web import Request, Response
 from zabbix import get_zabbix_clients
 
 
-def json_response(handler):
-    async def wrapper(*args, **kwargs):
-        data = await handler(*args, **kwargs)
-        return web.json_response(data)
-
+def json_service(handler):
+    async def wrapper(request: Request, *args, **kwargs):
+        request_data = await request.json()
+        response_data = await handler(request_data)
+        return web.json_response(response_data)
     return wrapper
 
-@json_response
-async def index(request: Request):
-    data = await request.json()
-    query = data.get('query')
-    clients = await get_zabbix_clients()
-    for client in clients:
-        hosts = await client.get_hosts()
-        print(hosts)
-        print()
+
+@json_service
+async def index(request_data: dict):
+        query = request_data.get('query')
+        clients = await get_zabbix_clients()
+        for client in clients:
+            hosts = await client.get_hosts()
+        return hosts
