8b92d26cc63e804e68495cc62f2d3c28f4c680a0
mmanylov <maksim.manylov@ramax.ru>
2020-07-13 15:36:31 +0300

    APP [FIX]: рефакторинг клиента Zabbix

--- zabbix.py
+++ zabbix.py
@@ -36,35 +36,34 @@ class ZabbixClient:
     def __init__(self, server_url):
         self.server_url = server_url
 
-    async def authorize(self):
+    async def call_api(self, json_payload):
         async with ClientSession() as session:
-            async with session.post(self.server_url, json=ZABBIX_STANDARD_AUTH) as resp:
+            async with session.post(self.server_url, json=json_payload) as resp:
                 resp = await resp.json()
-                token = resp.get('result')
-                if token:
-                    self.auth_token = token
+                return resp.get('result')
+
+    async def authorize(self):
+        token = await self.call_api(ZABBIX_STANDARD_AUTH)
+        if token:
+            self.auth_token = token
 
     async def get_hosts(self):
         hosts = list()
-        for url in ZABBIX_URLS:
-            async with ClientSession() as session:
-                async with session.post(url, json=get_zabbix_hosts_get_payload(self.auth_token)) as resp:
-                    resp_json = await resp.json()
-                    if len(resp_json.get('result')) > 0:
-                        for host in resp_json.get('result'):
-                            host_data = {
-                                'hostid': host.get('hostid'),
-                                'host': host.get('host'),
-                                'name': host.get('name'),
-                            }
+        result = await self.call_api(get_zabbix_hosts_get_payload(self.auth_token))
+        for host in result:
+            host_data = {
+                'hostid': host.get('hostid'),
+                'host': host.get('host'),
+                'name': host.get('name'),
+            }
 
-                            interfaces = host.get('interfaces')
-                            ips = list()
-                            for interface in interfaces:
-                                ips.append(interface.get('ip'))
+            interfaces = host.get('interfaces')
+            ips = list()
+            for interface in interfaces:
+                ips.append(interface.get('ip'))
 
-                            host_data['ips'] = ips
-                            hosts.append(host_data)
+            host_data['ips'] = ips
+            hosts.append(host_data)
 
         return hosts
 
